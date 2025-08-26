c15cf430bf918d904328f083e7d018d96af9c6ff
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2025-08-26 10:36:51 +0000

    test controller

---
+++ MyWeb/app/Http/Controllers/DaiController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DaiController extends Controller
+{
+    public function index(){
+        
+    return ('DaiView');
+    }
+    
+}
---
+++ MyWeb/resources/views/DaiView.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- It always seems impossible until it is done. - Nelson Mandela -->
+
+    <p>ban Dai say hello</p>
+</div>
